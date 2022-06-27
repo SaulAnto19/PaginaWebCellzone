5639a87a63865853b6f356b70a67c4fef244f80e
Saúliño <48542710+SaulAnto19@users.noreply.github.com>
2022-06-26 19:36:32 -0600

    add barra dinamica y estatica a la ves agregando eventos jquery agregando mapa de la ubicacion de la tienda estamos a un 85% del proyecto

---
+++ js/main.js
@@ -0,0 +1,101 @@
+var URLactual = document.title;
+
+$(function()
+{
+	'use strict';
+	
+	/*Mapa*/
+	var home= "Cellzone";
+	var visitenos= "Visitenos";
+	
+	if(URLactual === home || URLactual === visitenos){
+	   	
+		var map= L.map('mapa').setView([12.125817722849822, -86.2542873316622], 30);
+
+		
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		L.marker([12.125817722849822, -86.2542873316622]).addTo(map)
+			.bindPopup('TIENDA CELLZONE.')
+			.openPopup();
+	}
+});
+
+$(function()
+{
+		'use strict';
+	
+	
+	var windoHeight = $('#headerDinamic').height();
+	var barraAltura = $('.barra').innerHeight();
+
+	
+	
+	$(window).scroll(function()
+	{
+		var scroll = $(window).scrollTop();
+		
+		if(scroll > windoHeight)
+		{
+			$('.barra').addClass('fixed');
+			$('body').css({'margin-top' : barraAltura+'px'});
+		}else
+		{
+			$('.barra').removeClass('fixed');
+			
+			$('body').css({'margin-top' : '0px'});
+		}
+		
+	});
+	
+	icon();
+	
+});
+
+function icon()
+{
+	/*Los del header*/
+	/*Facebook*/
+	$(".faceH").hover(function(){
+		
+		$(".faceH").animate(
+			{
+			 color:'#3b5998'
+			},500);
+	}, function(){
+		$(".faceH").animate(
+			{
+			 color:'#fff'
+			},500);
+	});
+	
+	/*twitter*/
+ 	$(".twitH").hover(function(){
+		
+		$(".twitH").animate(
+			{
+			 color:'#00acee'
+			},500);
+	}, function(){
+		$(".twitH").animate(
+			{
+			 color:'#fff'
+			},500);
+	});
+	
+	/*Instagram*/
+	 	$(".instH").hover(function(){
+		
+		$(".instH").animate(
+			{
+			 color:'#E1306C'
+			},500);
+	}, function(){
+		$(".instH").animate(
+			{
+			 color:'#fff'
+			},500);
+	});
+}
