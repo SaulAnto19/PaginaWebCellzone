ac04eff258fb5d480ac00ddde5837592d1a5f019
Saúliño <48542710+SaulAnto19@users.noreply.github.com>
2022-07-02 17:27:29 -0600

    add roles de usuario ventana de home de empleado ya casi terminado

--- js/validation.js
+++ js/validation.js
@@ -2,8 +2,13 @@ var btn = document.getElementById("sesion");
 
 console.log(btn);
 
+/* Administrador*/
 var c = 1234;
 var u = "Admin";
+
+/* Empleado */
+var cE = 12345678;
+var uE = "Allison";
 
 btn.addEventListener("click", function (evt) {
   if (document.form.clave.value == c && document.form.usuario.value == u) {
@@ -11,9 +16,14 @@ btn.addEventListener("click", function (evt) {
     window.location = "/html/comprasYsesion/homeAdmin.html";
     evt.preventDefault();
     return false;
+  } else if (document.form.clave.value == cE && document.form.usuario.value == uE) {
+    alert("Bienvenidos " + uE + " en su inicio de sesion correctamente");
+    window.location = "/html/comprasYsesion/homeEmpleado.html";
+    evt.preventDefault();
+    return false;
   } else {
     alert("por favor ingrese nuevamente su contraseña y usuario");
     evt.preventDefault();
     return false;
-  }
+  } 
 });
