77da87a7642d3a69e261fe86567b12ebfa04067c
Saúliño <48542710+SaulAnto19@users.noreply.github.com>
2022-07-01 23:23:22 -0600

    Add la validacion de inicio de sesion en nuestra pagina web falta el de agregar usuarios casi completo

---
+++ js/validation.js
@@ -0,0 +1,19 @@
+var btn = document.getElementById("sesion");
+
+console.log(btn);
+
+var c = 1234;
+var u = "Admin";
+
+btn.addEventListener("click", function (evt) {
+  if (document.form.clave.value == c && document.form.usuario.value == u) {
+    alert("Bienvenidos " + u + " en su inicio de sesion correctamente");
+    window.location = "/html/comprasYsesion/addCell.html";
+    evt.preventDefault();
+    return false;
+  } else {
+    alert("por favor ingrese nuevamente su contraseña y usuario");
+    evt.preventDefault();
+    return false;
+  }
+});
